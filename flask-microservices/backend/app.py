a1da827a7825847768ec1d47ceea06b55374e73f
SurajRane <121447350+RaneSuraj@users.noreply.github.com>
2026-08-26 22:27:20 +0530

    Update app.py -- Import order correction
    as per the linter corrected my import order

--- flask-microservices/backend/app.py
+++ flask-microservices/backend/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 import os
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
